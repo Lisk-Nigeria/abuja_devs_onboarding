d78fe25eb6be09a3078bb3f7e140a22d9578cf22
juto-shogan <somtombonu53.com>
2025-03-19 10:31:26 +0100

    started

---
+++ mbonu_somto_submission.js
@@ -0,0 +1,39 @@
+exports.reverseString = function(s) {
+  // Your implementation here
+};
+
+exports.isPalindrome = function(s) {
+  // Your implementation here
+};
+
+exports.fibonacci = function(n) {
+  // Your implementation here
+};
+
+exports.factorial = function(n) {
+  // Your implementation here
+};
+
+exports.sumArray = function(arr) {
+  // Your implementation here
+};
+
+exports.maxArray = function(arr) {
+  // Your implementation here
+};
+
+exports.mergeSortedArrays = function(arr1, arr2) {
+  // Your implementation here
+};
+
+exports.uniqueArray = function(arr) {
+  // Your implementation here
+};
+
+exports.countVowels = function(s) {
+  // Your implementation here
+};
+
+exports.evenOrOdd = function(n) {
+  // Your implementation here
+};
